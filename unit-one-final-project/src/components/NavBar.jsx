72edd5fa638d500bcc08047bbf9a29c477681a18
BryJime <bry.jimenex@gmail.com>
2025-07-16 15:46:41 -0700

    Added Navigation Bar

---
+++ unit-one-final-project/src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+
+
+function NavBar() {
+
+
+    return (
+        <>
+    
+            <section className="header">
+                <section className="header-logo">
+                    <h1 className="logo">TITLE</h1>
+                </section>
+
+            <section className="navbar">
+                <section className="header-home">
+                    <Link to="/">
+                        HOME
+                    </Link>
+                </section>
+
+                <section className="header-shortcuts">
+                    <Link to="/Shortcuts.jsx">
+                        SHORTCUTS
+                    </Link>
+                </section>
+
+                <section className="header-about">
+                    <Link to="/About.jsx">
+                        ABOUT
+                    </Link>
+                </section>
+            </section>
+
+            </section>
+
+        </>
+    )
+}
+
+export default NavBar;
